4fba7c1b743e94e61c7cdaa86d663c0f5d52b434
Matheus-Cardoso <matheuscardoso86@hotmail.com>
2025-07-22 12:30:08 -0300

    Configure app para funcionar em /pokedex/ - base href correto

---
+++ build-for-subpath.js
@@ -0,0 +1,21 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+// Build da aplicação
+console.log('Building Angular application...');
+execSync('ng build --configuration=production --base-href=/pokedex/', { stdio: 'inherit' });
+
+// Modificar arquivos para funcionar em subpath
+console.log('Modifying files for subpath deployment...');
+
+const indexPath = path.join(__dirname, 'www', 'index.html');
+let indexContent = fs.readFileSync(indexPath, 'utf8');
+
+// Corrigir paths dos recursos que não começam com /pokedex/
+indexContent = indexContent.replace(/href="(?!\/pokedex\/|http|\/\/)/g, 'href="/pokedex/');
+indexContent = indexContent.replace(/src="(?!\/pokedex\/|http|\/\/)/g, 'src="/pokedex/');
+
+fs.writeFileSync(indexPath, indexContent);
+
+console.log('Build completed and configured for /pokedex/ subpath');
